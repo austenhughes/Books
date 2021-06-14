a545d7ab1d2c22a8e8fd4d1445008bfa7f212334
Austn Hughes <austen.hughes.91@gmail.com>
2021-06-14 13:05:31 -0600

    changing back

--- server.js
+++ server.js
@@ -9,21 +9,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "/client/build")));
-  // Handle React routing, return all requests to React app
-  // app.get("*", function (req, res) {
-  //   res.sendFile(path.join(__dirname, "/client/build/index.html"));
-  // });
+  app.use(express.static("client/build"));
 }
+
+// // The "catchall" handler: for any request that doesn't
+// // match one above, send back React's index.html file.
+// if (process.env.NODE_ENV === "production") {
+//   // Serve any static files
+//   app.use(express.static(path.join(__dirname, "/client/build")));
+//   // Handle React routing, return all requests to React app
+//   app.get("*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "/client/build/index.html"));
+//   });
+// }
 
 
 // Add routes, both API and view
