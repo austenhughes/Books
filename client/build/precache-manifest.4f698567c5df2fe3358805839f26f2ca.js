f8ed7f50078db5038af1a2013e836bc28e7f8e5f
Austn Hughes <austen.hughes.91@gmail.com>
2021-06-13 18:58:54 -0600

    working on heroku

---
+++ client/build/precache-manifest.4f698567c5df2fe3358805839f26f2ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d032668db31eca8e1c065447a96b667",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1030c89ca5949c23dafa",
+    "url": "/static/css/2.02adba32.chunk.css"
+  },
+  {
+    "revision": "d80e4dd29da3d5334a1e",
+    "url": "/static/css/main.40326349.chunk.css"
+  },
+  {
+    "revision": "1030c89ca5949c23dafa",
+    "url": "/static/js/2.196d0998.chunk.js"
+  },
+  {
+    "revision": "8c3ba619beac464b6e5a301bc44e2ca3",
+    "url": "/static/js/2.196d0998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d80e4dd29da3d5334a1e",
+    "url": "/static/js/main.05094ff3.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
